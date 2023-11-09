ec00982853c003656574ed91f70db2f0c2fd0949
teejay009 <tajdjimoh@gmail.com>
2023-11-09 11:12:36 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,55 @@
 import { useState, useEffect, useCallback, useMemo } from "react";
 
-const getArray = () =>{
-  for(let i = 0; i < 1000000000; i++) {
+import React from "react";
 
-  }return ['Muhammad', "Good Boy"]
+const getArray = () => {
+  for(let i = 0; i < 100000000; i++) {
+
+  }
+  return ['Muhammad', "Fine Boy"]
 }
 
-import React from 'react'
-
 const App = () => {
-  const [userInput, setUserInput] = useState('')
-  const [randomInput,setRandomInput] = useState("")
+  const [userInput, setUserInput] = useState("");
+  const [randomInput, setRandomInput] = useState("");
 
   const fib = useCallback((n) => {
-    return n <= 1 ? n : fib(n - 1) + fib(n - 2)
+    return n <= 1 ? n : fib(n-1) + fib(n-2)  
+
   }, [])
 
-  const fibNumber = useMemo(() => fib(userInput), [userInput, fib]);
+
+
+  const fibNumber = useMemo(() => fib(userInput), [userInput, fib])
 
   const myArray = useMemo(() => getArray(), [])
-
   useEffect(() => {
     console.log("New Array");
   }, [myArray])
 
-  
-  
-  return (
 
+
+
+  return(
     <main className="App">
-      <label> Fibonacci Sequence:</label>
-      <input type="text"
-       placeholder="Position"
-      value={userInput}
-       onChange={(e) => setUserInput(e.target.value)}
-      />
+      <label htmlFor="">Fibonacci Sequence</label>
+      <input type="text" placeholder="position" value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
 
-      <P>Number: {fibNumber || "--"} </P>
-      
-
+      <p>Number: {fibNumber || "--"}</p>
       <br />
       <br />
 
-      <label>Random Input:</label>
+      <label htmlFor="">Random Input:</label>
       <input type="text" 
       value={randomInput}
-      placeholder="thanks you are welcome"
+      placeholder='Type anything...'
       onChange={(e) => setRandomInput(e.target.value)}
-      
       />
       <p>Result: {randomInput}</p>
-      
-
-      
 
     </main>
-    
   )
+
 }
 
-export default App
+export default App;
